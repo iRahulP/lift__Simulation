416959b9f88d250b645e6062a5aacf5ba98ecb2b
Rahul Patil <rahulpatil.m30@gmail.com>
2021-08-30 13:48:00 -0400

    updated Lift Simulation

--- src/components/lift.js
+++ src/components/lift.js
@@ -1,29 +1,112 @@
 import React from 'react'
 import { useState } from 'react';
-import Floor from './Floor';
 import './Lift.css';
 
-const Lift = () => {
+const Lift = (props) => {
+    const [currentFloor, setCurrentFloor] = useState('0');
+
+    function moveFloors(e) {
+        const currentFloor = e.target.id;
+        setCurrentFloor(currentFloor);
+    }
+    
     return (
-      <div>
-        <section className="floorSection">
-          <Floor
-            level='4'
-            moveFloors
-          />
-          <Floor 
-            level='3'
-            moveFloors
-          />
-          <Floor 
-            level='2'
-            moveFloors
-          />
-          <Floor 
-            level='1'
-            moveFloors
-          />
-        </section>
+      <div className="floorsContainer">
+        <div className="floor">
+          <div>
+            <button
+              className="button b1"
+              id="4"
+              onClick={(e) => moveFloors(e)}
+            >
+              Down
+            </button>
+          </div>
+          <h3>L4</h3>
+        </div>
+
+        <div className="floor">
+          <div>
+            <button
+              className="button b2"
+              id="3"
+              onClick={(e) => moveFloors(e)}
+            >
+              Up
+            </button>
+            <br />
+            <button
+              className="button b1"
+              id="3"
+              onClick={(e) => moveFloors(e)}
+            >
+              Down
+            </button>
+          </div>
+          <h3>L3</h3>
+        </div>
+
+        <div className="floor">
+          <div>
+            <button
+              className="button b2"
+              id="2"
+              onClick={(e) => moveFloors(e)}
+            >
+              Up
+            </button>
+            <br />
+            <button
+              className="button b1"
+              id="2"
+              onClick={(e) => moveFloors(e)}
+            >
+              Down
+            </button>
+          </div>
+          <h3>L2</h3>
+        </div>
+
+        <div className="floor">
+          <div>
+            <button
+              className="button b2"
+              id="1"
+              onClick={(e) => moveFloors(e)}
+            >
+              Up
+            </button>
+            <br />
+            <button
+              className="button b1"
+              id="1"
+              onClick={(e) => moveFloors(e)}
+            >
+              Down
+            </button>
+          </div>
+          <h3>L1</h3>
+        </div>
+
+        <div className="floor">
+          <div>
+            <button
+              className="button b2"
+              id="0"
+              onClick={(e) => moveFloors(e)}
+            >
+            Up
+            </button>
+          </div>
+          <h3>B</h3>
+      
+          <div
+            className="lift"
+            style={{ transform: `translateY(${-currentFloor * "140"}px)` }}
+          >
+          </div>
+        </div>
+
       </div>
     )
 }
